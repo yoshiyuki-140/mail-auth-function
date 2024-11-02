9b239f62f31d119d351b43eab84fc9fdc9aabd8b
Yoshiyuki Kurose <c1221251@st.kanazawa-it.ac.jp>
2024-11-03 00:17:40 +0900

    update : フォーマッターにかけた

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 dialect = "postgresql"
 driver = "psycopg2"
