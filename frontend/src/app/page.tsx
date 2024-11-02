b100fd1bb4beeea4fc4ce1b6be8b19902422f9f0
Yoshiyuki Kurose <c1221251@st.kanazawa-it.ac.jp>
2024-11-02 19:50:37 +0900

    add : 画面遷移機能を追加

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,13 @@
+'use client';
+
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleNavigation = () => {
+    router.push("遷移先のパス");
+  }
   return (
     <div className="flex items-center justify-center h-screen w-screen bg-white">
       <div className="text-white p-4 h-screen flex flex-col">
@@ -9,7 +17,10 @@ export default function Home() {
           >認証システムデモへようこそ</h1>
         </div>
         <div className="flex justify-center mt-40">
-          <button className="text-black border bg-red-500 px-8 py-2 shadow-lg rounded-3xl hover:bg-red-400">
+          <button
+            onClick={handleNavigation}
+            className="text-black border bg-red-500 px-8 py-2 shadow-lg rounded-3xl hover:bg-red-400"
+          >
             登録
           </button>
         </div>
