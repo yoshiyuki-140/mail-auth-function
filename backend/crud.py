38d603572448999d3af10a11b5f41c674275337a
Yoshiyuki Kurose <c1221251@st.kanazawa-it.ac.jp>
2024-11-03 22:29:09 +0900

    add : DBのcrud.pyに一時保存テーブルにユーザー情報を保存する関数、トークンによって一時保存テーブルの情報を返す関数、一時保存テーブルからuser_idによってデータを削除する関数の3つの関数を追加

--- backend/crud.py
+++ backend/crud.py
@@ -1,6 +1,7 @@
 from sqlalchemy import text
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Session
+from token import create_token
 
 
 # CREATE
@@ -17,11 +18,11 @@ def create_user(db: Session, name: str, email: str, password: str):
     try:
         result = db.execute(q, params)
         db.commit()
-        return result
+        return True
     except SQLAlchemyError as e:
         db.rollback()
         print("Error occurred:", e)
-        return None
+        return False
 
 
 # READ
@@ -117,3 +118,103 @@ def delete_user(db: Session, user_id: int):
         db.rollback()
         print("Error occured:", e)
         return None
+
+
+# ユーザー情報を一時保存テーブルに保存する
+def create_temporary_user(db: Session, name: str, email: str, password: str):
+    """ユーザー作成を行う関数"""
+    # ここでトークンを作成する
+    token = create_token()
+
+    # ユーザー情報一時保存テーブルにユーザー情報を保存する
+    q = text(
+        "insert into temporary_users (name,email,password,token) values (:name, :email, :password, :token)"
+    )
+    params = {
+        "name": name,
+        "email": email,
+        "password": password,
+        "token": token,
+    }
+    try:
+        result = db.execute(q, params)
+        db.commit()
+        return result
+    except SQLAlchemyError as e:
+        db.rollback()
+        print("Error occurred:", e)
+        return None
+
+
+# トークン情報を認証して一時保存テーブルからユーザー情報保存テーブルに情報を保存する
+# トークンが一致しなければFalseを返す
+def auth_temporary_user_by_token(db: Session, email: str, token: int):
+    """トークンからemailの情報を
+
+    Args:
+        db (Session): _description_
+        email (str): _description_
+        token (int): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    q = text("select token,id,name,password from temporary_users where email = :email")
+    params = {"email": email}
+    try:
+        result = db.execute(q, params)  # クエリ実行
+        row = result.fetchone()  # クエリ実行結果から一行取得する
+        if row:
+            acquired_token = row[0]
+            id = row[1]
+            name = row[2]
+            password = row[3]
+            if acquired_token == token:
+                # 認証が成功した時の処理
+                ## ユーザーをusersに作る
+                if create_user(db, name, email, password):
+                    ## temporary_usersテーブルからuserデータを削除する
+                    if delete_temporary_user(user_id=id):
+                        return True
+                    else:
+                        print(
+                            "Error:ユーザー情報一時保存テーブルの情報を消せませんでした"
+                        )
+                        return False
+                else:
+                    print("Error:ユーザーテーブルにデータを作成することに失敗しました")
+                    return False
+            else:
+                print("Error:認証が失敗しました")
+                return False
+        else:
+            print("DBからtokenが取ってこれませんでした。")
+            return False
+    except SQLAlchemyError as e:
+        print("Error occurred:", e)
+        return False
+
+
+# ここに一時情報保存テーブルからidによりデータを削除する関数を書く
+def delete_temporary_user(db: Session, user_id: int):
+    """ユーザー情報一時保存テーブルからユーザデータを削除する関数
+
+    Args:
+        db (Session): _description_
+        user_id (int): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    q = "delete from temporary_users where id = :id"
+    params = {"id": user_id}
+    try:
+        result = db.execute(q, params)
+        db.commit()
+        # 消された要素の数を表示
+        print(f"Deleted {result.rowcount}")
+        return True
+    except SQLAlchemyError as e:
+        db.rollback()
+        print("Error occured:", e)
+        return False
