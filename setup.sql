5b752b361c8bb1d1c59d88d01ea7ef92d20b5f91
Yoshiyuki Kurose <c1221251@st.kanazawa-it.ac.jp>
2024-11-03 20:59:37 +0900

    add : ユーザー情報一時保存テーブルをsetup.sqlに追加した

--- setup.sql
+++ setup.sql
@@ -10,5 +10,14 @@ CREATE TABLE users (
     password VARCHAR(255)
 );
 
+-- ユーザー情報を認証トークンとともに一時的に保存するテーブルを作成する。
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255),
+    token CHAR(6) NOT NULL,
+);
+
 -- テストコードのためのDB
 CREATE DATABASE test_db;
