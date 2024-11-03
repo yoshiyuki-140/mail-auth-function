80b691e127b536c0bb7d07575507c49fe06af52f
Yoshiyuki Kurose <c1221251@st.kanazawa-it.ac.jp>
2024-11-03 22:34:54 +0900

    add :
    - ユーザー情報を一時保存テーブルに保存するためのエントリポイントを追加
    -
    emailとtokenからtemporary_usersテーブルにある情報をusersテーブルに登録するためのエントリポイントを追加

--- backend/main.py
+++ backend/main.py
@@ -49,8 +49,10 @@ def create_user(
     db: Session = Depends(get_db),
 ):
     # ユーザー作成
-    crud.create_user(db, body.name, body.email, body.password)
-    return {"Success": "user was created"}
+    if crud.create_user(db, body.name, body.email, body.password) == True:
+        return {"code": "0", "Message": "Success"}
+    else:
+        return {"code": "-1", "Message": "Failed"}
 
 
 # READ
@@ -110,3 +112,40 @@ def delete_user(user_id: int, db: Session = Depends(get_db)):
     """
     crud.delete_user(db, user_id)
     return {"StatusMessage": "Success Delete Params"}
+
+
+#################### ユーザー情報一時保存テーブルに情報を保存する ####################
+
+
+@app.post("/temporary_user/")
+def add_temporary_user_info(
+    body: schemas.TemporaryUserInformation, db: Session = Depends(get_db)
+):
+    """ユーザー情報一時保存テーブルに新しいユーザー情報とトークンを作成する
+
+    Args:
+        body (schemas.TemporaryUserInformation): _description_
+        db (Session, optional): _description_. Defaults to Depends(get_db).
+    """
+    crud.create_temporary_user(db, body.name, body.email, body.password)
+    return {"Success": "temporary user was stored"}
+
+
+# トークン情報を認証して一時保存テーブルからユーザー情報保存テーブルに情報を保存するエントリポイント
+@app.post("/temporary_user/auth/")
+def auth_temporary_user(
+    body: schemas.AuthTemporaryUserInformation, db: Session = Depends(get_db)
+):
+    """リクエストボディーからトークンを取得し、一時保存テーブルと同じなら本ユーザーテーブルにユーザー情報を登録し、サインアップを完了するためのエントリポイント
+
+    Args:
+        body (schemas.AuthTemporaryUserInformation): _description_
+        db (Session, optional): _description_. Defaults to Depends(get_db).
+    """
+    result = crud.auth_temporary_user_by_token(db, body.email, body.token)
+    if result:
+        # 認証に成功した時
+        return {"code": "0", "Message": "Success"}
+    else:
+        # 認証に失敗した時
+        return {"code": "-1", "Message": "Failed"}
